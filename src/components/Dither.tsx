20a07a31dbbff3af015790f20553516d441c07ed
Heizle <69576473+gnlwjd231@users.noreply.github.com>
2026-07-09 23:00:09 +0900

    Fix ThreeEvent import error for @react-three/fiber v9

--- src/components/Dither.tsx
+++ src/components/Dither.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect, forwardRef } from 'react';
-import { Canvas, useFrame, useThree, ThreeEvent } from '@react-three/fiber';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { EffectComposer, wrapEffect } from '@react-three/postprocessing';
 import { Effect } from 'postprocessing';
 import * as THREE from 'three';
@@ -252,7 +252,7 @@ function DitheredWaves({
     }
   });
 
-  const handlePointerMove = (e: ThreeEvent<PointerEvent>) => {
+  const handlePointerMove = (e: any) => {
     if (!enableMouseInteraction) return;
     const rect = gl.domElement.getBoundingClientRect();
     const dpr = gl.getPixelRatio();
